eb745b5129a4aa87718244a44e0d71a5fd55888a
Jonathan MacDuff <jonathan.macduff@outlook.com>
2023-12-02 22:21:44 -0800

    navbar, images file, and other tweaks

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "A majestic otter lounges by the seaside";
 document.querySelector("body").appendChild(h2);
